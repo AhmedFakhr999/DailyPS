b7b59c771e0b3549d19c77acc364c7c631368160
76AHMEDFAKHER <220210469@fa-hists.edu.eg>
2026-08-20 18:37:24 +0300

    update solution for time limit exceed

--- Light_more_light.cpp
+++ Light_more_light.cpp
@@ -8,13 +8,12 @@
 #include <set>
 #include <vector>
 
+const long long Maxy = 1e6;
 
-const long long Maxy=1e6;
-
-#define fasttoo                                                                \
-  ios_base::sync_with_stdio(0);                                                \
-  cin.tie(0);                                                                  \
-  cout.tie(0);
+#define fasttoo                   \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);                   \
+	cout.tie(0);
 #define ll long long
 
 using namespace std;
@@ -24,40 +23,28 @@ const int MAX_SIZE = 1e3;
 char arr[MAX_SIZE][MAX_SIZE];
 const int INF = 1e9;
 int visit[MAX_SIZE][MAX_SIZE] = {
-    0}; // Ensure visit is initialized and globally accessible
+	0}; // Ensure visit is initialized and globally accessible
 vector<pair<pair<int, int>, int>> res;
 int n, m;
 const int sz = 1e6 + 10;
 bool composite[sz + 1];
-vector<int> prime; 
-vector<bool>seive_prime(Maxy,true);
-vector<bool>linear_prime(Maxy,true);
-vector<ll>spf(Maxy);
-int main(){
-    fasttoo
-  int x;
-	cin >> x;
-	
-	while (x != 0) {
-		int count = 0;
-		for (int i = 1; i <= x; i++) {
-			if (x % i == 0)
-				count++;
-
+vector<int> prime;
+vector<bool> seive_prime(Maxy, true);
+vector<bool> linear_prime(Maxy, true);
+vector<ll> spf(Maxy);
+int main()
+{
+	fasttoo unsigned int n;
+	while (cin >> n && n != 0)
+	{
+		unsigned int sqr = sqrt(n);
+		if (sqr * sqr == n)
+		{
+			cout << "yes" << endl;
 		}
-		if (count % 2 == 0)
-			cout << "no\n";
 		else
-
-			cout << "yes\n";
-		cin >> x;
-
-
-
-
-
+		{
+			cout << "no" << endl;
+		}
 	}
-
-		return 0;
-    
 }
